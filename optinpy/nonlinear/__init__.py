ef403eacf27e7ad1f55c3c4df0028cc1a5b4883d
brian.nghiem <brian.nghiem@mail.utoronto.ca>
2021-01-08 01:05:02 -0500

    cupy/numpy agnostic

--- optinpy/nonlinear/__init__.py
+++ optinpy/nonlinear/__init__.py
@@ -3,10 +3,10 @@ from __future__ import division, absolute_import, print_function
 
 from .unconstrained import unconstrained as __unconstrained
 from .constrained import constrained as __constrained
-from .. import np as __np
+from .. import xp as __xp
 
-__eps = __np.finfo(__np.float64).eps
-__resolution = __np.finfo(__np.float64).resolution
+__eps = __xp.finfo(__xp.float64).eps
+__resolution = __xp.finfo(__xp.float64).resolution
 
 params = {'fminunc':{'method':'newton','params':\
                 {'gradient':{'max_iter':1e3},\
@@ -23,14 +23,14 @@ params = {'fminunc':{'method':'newton','params':\
                 {'log-barrier':{'max_iter':1e3,'threshold':1e-6},\
                  'barrier':{'max_iter':1e3,'threshold':1e-6},\
                  'penalty':{'max_iter':1e3,'threshold':1e-6},\
-                 }},  
-          'jacobian':{'algorithm':'central','epsilon':__np.sqrt(__eps)},\
-          'hessian':{'algorithm':'central','epsilon':__np.sqrt(__eps),'initial':None},\
+                 }},
+          'jacobian':{'algorithm':'central','epsilon':__xp.sqrt(__eps)},\
+          'hessian':{'algorithm':'central','epsilon':__xp.sqrt(__eps),'initial':None},\
           'linesearch':{'method':'backtracking','params':
                 {'backtracking':{'alpha':1,'rho':0.6,'c':1e-3,'max_iter':1e3},\
                 'interp23':{'alpha':1,'alpha_min':0.1,'rho':0.6,'c':1e-3,'max_iter':1e3},\
                 'unimodality':{'b':1,'threshold':1e-4,'max_iter':1e3},
-                'golden-section':{'b':1,'threshold':1e-4,'max_iter':1e3}}}} 
+                'golden-section':{'b':1,'threshold':1e-4,'max_iter':1e3}}}}
 
 
 unconstrained = __unconstrained(params)
